3fa556bcb91a4e2a5e0ec4f258de1e741a2549b1
deadpanda <clement.lagier@epitech.eu>
2026-01-23 15:42:58 +0100

    [ADD] Added a db.py class to make the sqlite3 db more abstract

---
+++ db.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sqlite3
+
+class Database(object):
+    def __init__(self, filename):
+        self.conn = sqlite3.connect(filename)
+        self.cursor = self.conn.cursor()
+        
+        self.conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS alerts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                ts INTEGER NOT NULL,
+                src_ip TEXT NOT NULL,
+                alert_type TEXT NOT NULL,
+                details TEXT
+            )
+            """
+        )
+        conn.commit()
+    def add_alert(self, ts, src_ip, alert_type, details):
+        self.cursor.execute(
+            """
+            INSERT INTO alerts (ts, src_ip, alert_type, details)
+            VALUES (?, ?, ?, ?)
+            """,
+            (ts, src_ip, alert_type, details)
+        )
+        self.conn.commit()
+
