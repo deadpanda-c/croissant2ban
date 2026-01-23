bac3cfc4a4adabd56a8f36f52ee41b255b135ae9
deadpanda <clement.lagier@epitech.eu>
2026-01-23 11:43:26 +0100

    [UPDATE] Init the config from conf/croissant.json

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import errno
 import sys
 from scapy.all import sniff, IP, TCP, UDP, Raw
 import os
+import json
 
 def check_root():
     if os.getuid() != 0:
         print("Must be run as root", file=sys.stderr)
-        sys.exit(errno.EPERM)
+        raise SystemExit(errno.EPERM)
 
 def handle_packet(pkt):
     if IP not in pkt or TCP not in pkt:
@@ -17,17 +18,28 @@ def handle_packet(pkt):
     tcp = pkt[TCP]
     print(ip.src, ip.dst, tcp.sport, tcp.dport)
 
+def init_config():
+    with open('conf/croissant.json') as f:
+        config = json.load(f)
+
+    for service in config['services'].values():
+        try:
+            if service['enabled'] and service['enabled'] == True:
+                print("Starting service...")
+        except KeyError:
+            print("Service not enabled")
+            continue
 
 if __name__ == '__main__':
     check_root()
     try:
-        sniff(
-                # sniff localhost only
-                iface="lo",
-                filter="ip and tcp and host 127.0.0.1",
-                prn=handle_packet,
-                store=0,
-                )
+        init_config()
+        # sniff(
+        #         iface="lo",
+        #         filter="ip and tcp and host 127.0.0.1",
+        #         prn=handle_packet,
+        #         store=0,
+        #         )
     except KeyboardInterrupt:
         pass
     
