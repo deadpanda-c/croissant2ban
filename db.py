8cd9115d3978d8157b3dc0c19f2269ab8830602b
Clément Lagier <clement.lagier@epitech.eu>
2026-04-24 10:32:43 +0200

    [FIX] Closed db connection

--- db.py
+++ db.py
@@ -2,13 +2,13 @@
 
 import sqlite3
 
+
 class Database(object):
     def __init__(self, filename):
         self.conn = sqlite3.connect(filename)
         self.cursor = self.conn.cursor()
-        
-        self.conn.execute(
-            """
+
+        self.conn.execute("""
             CREATE TABLE IF NOT EXISTS alerts (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 ts INTEGER NOT NULL,
@@ -16,16 +16,18 @@ class Database(object):
                 alert_type TEXT NOT NULL,
                 details TEXT
             )
-            """
-        )
+            """)
         self.conn.commit()
+
     def add_alert(self, ts, src_ip, alert_type, details):
         self.cursor.execute(
             """
             INSERT INTO alerts (ts, src_ip, alert_type, details)
             VALUES (?, ?, ?, ?)
             """,
-            (ts, src_ip, alert_type, details)
+            (ts, src_ip, alert_type, details),
         )
         self.conn.commit()
 
+    def close(self):
+        self.conn.close()
